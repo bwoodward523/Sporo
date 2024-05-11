50b4dc49f742c24eb774c4bfa156c9da8b78b263
emmetl913 <elars1002@gmail.com>
2024-05-10 23:37:00 -0400

    PROCEDURAL HEATMAP LETS GOO

--- Scenes/TileMap.gd
+++ Scenes/TileMap.gd
@@ -3,8 +3,8 @@ extends TileMap
 var moisture = FastNoiseLite.new()
 var temperature = FastNoiseLite.new()
 var altitude = FastNoiseLite.new()
-var width = 16
-var height = 16
+var width = 20
+var height = 20
 @onready var player = get_parent().get_node("player")
 
 func _ready():
@@ -21,11 +21,9 @@ func _process(delta):
 func generate_chunk(position):
 	
 	var tile_pos = local_to_map(position)
-	print("Tile X: ",tile_pos.x," Tile Y: ",tile_pos.y)
 	for x in range(width):
 		for y in range(height):
-			var moist = moisture.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 +y)
-			var temp = temperature.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 +y)
-			var alt = altitude.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 +y)
-			#set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2+y), )
-			set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2+y), 3, Vector2i(0,0))
+			var moist = moisture.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 +y)*10
+			var temp = temperature.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 +y)*10
+			var alt = altitude.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 +y)*10
+			set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2+y), 0, Vector2(round((moist+10)/5), round((temp+10)/5)))
