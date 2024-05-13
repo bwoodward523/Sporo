688dc8f7954e8ee9b0d4b1cdc59b817a992c5576
bwoodward523 <bwoodward@ycp.edu>
2024-05-13 18:28:30 -0400

    hehe

--- TestWorld.gd
+++ TestWorld.gd
@@ -13,11 +13,11 @@ func _process(delta):
 	var rng = RandomNumberGenerator.new()
 	rng.randomize()
 	
-	#for i in 10:
-		#if rng.randi_range(1, 2) == 2:
-			#var instance = raindrop.instantiate()
-			#var randWidth = rng.randi_range(player.position.x - screenWidth.x/2, player.position.x + screenWidth.x/2)
-			#instance.position = Vector2(randWidth, player.position.y - screenWidth.y/2)
-			#add_child(instance)
-	#
+	for i in 5:
+		if rng.randi_range(1, 2) == 2:
+			var instance = raindrop.instantiate()
+			var randWidth = rng.randi_range(player.position.x - screenWidth.x/2, player.position.x + screenWidth.x/2)
+			instance.position = Vector2(randWidth, player.position.y - screenWidth.y/2)
+			add_child(instance)
+	
 
